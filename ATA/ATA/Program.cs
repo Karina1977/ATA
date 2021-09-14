955d33ef362666d9538866a58c10a5bc64f48f60
Homer <Homer@DESKTOP-LKI2RBT>
2021-09-14 13:03:21 +0300

    Main2

--- ATA/ATA/Program.cs
+++ ATA/ATA/Program.cs
@@ -46,6 +46,62 @@ namespace ATA
 
             //--------------------------------------
 
+            
+
+            string userName3 = Console.ReadLine();
+
+            int myNumber1 = 15;
+            int myNumber2 = 21;
+
+            bool isEgualNumber = myNumber1 == myNumber2;
+
+            Console.WriteLine(isEgualNumber);
+
+            //-----------------------
+
+            int myNumber3 = 15;
+            int myNumber4 = 21;
+
+            bool isEgualNumber1 = myNumber3 < myNumber4;
+
+            Console.WriteLine(isEgualNumber);
+
+            ///------------------------------
+
+
+            int maxNumber1 = 20;
+            int maxNumber2 = 41;
+
+            int maxResult = Math.Max(maxNumber1, maxNumber2);
+
+            Console.WriteLine(maxResult);
+
+            //------------------------------
+
+            int minNumber3 = 3;
+            int minNumber4 = 41;
+
+            int minResult = Math.Max(minNumber3, minNumber4);
+
+            Console.WriteLine(maxResult);
+
+            ///-------------------------------------
+
+            int a = 9;
+            int b = 7;
+            int c;
+            c = a * b / 2;
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+            //---------------------------
+
+            double numberToRound = 12.456;
+            double Rounded = Math.Round(numberToRound, 1);
+            Console.WriteLine(Rounded);
+
+
+
 
         }
     }
